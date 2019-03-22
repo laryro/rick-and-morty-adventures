0da45f97d925cff5bfff83abeb08fbd2f5b62a97
Laryssa Rocha <laryssa.vrocha@gmail.com>
2019-03-22 11:52:30 -0300

    Updates

---
+++ precache-manifest.8159eb8f0e8884ab30f52b150a4561fb.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "301c6e2937a33a187cab",
+    "url": "/rick-and-morty-adventures/static/js/runtime~main.301c6e29.js"
+  },
+  {
+    "revision": "67e1cacb6d439accf816",
+    "url": "/rick-and-morty-adventures/static/js/main.67e1cacb.chunk.js"
+  },
+  {
+    "revision": "67cab99a6be649624872",
+    "url": "/rick-and-morty-adventures/static/js/2.67cab99a.chunk.js"
+  },
+  {
+    "revision": "67e1cacb6d439accf816",
+    "url": "/rick-and-morty-adventures/static/css/main.b46ea49f.chunk.css"
+  },
+  {
+    "revision": "3a6011a5429aa1b7bf4dbb216b3079f9",
+    "url": "/rick-and-morty-adventures/index.html"
+  }
+];
